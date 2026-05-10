ea4682659006c6801ac10269abd006c6f7d1c113
KollaUpendra <kollaupendra793@gmail.com>
2026-05-10 18:59:11 +0530

    completed

---
+++ file-organizer/watcher.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+WATCH_DIR="/home/ubuntu/down"
+SCRIPT="/home/ubuntu/file-organizer/organize.py"
+
+echo "[STARTED] Watching $WATCH_DIR"
+
+while read FILE
+do
+    FULL_PATH="$WATCH_DIR/$FILE"
+    echo "[EVENT] $FULL_PATH"
+
+    /usr/bin/python3 "$SCRIPT" "$FULL_PATH" &
+done < <(inotifywait -m -e close_write -e moved_to --format "%f" "$WATCH_DIR")
